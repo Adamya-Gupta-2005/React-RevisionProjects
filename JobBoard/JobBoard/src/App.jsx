44a6cf394c83832ef01cb955d35f25b9dceaf8ba
Adamya Gupta <adamyagupta005@gmail.com>
2026-09-20 20:19:09 +0530

    Job Board

---
+++ JobBoard/JobBoard/src/App.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import JobPosting from './components/JobPosting.jsx';
+
+const ITEMS_PER_PAGE = 6;
+const API_ENDPOINT = 'https://hacker-news.firebaseio.com/v0'
+
+const App = () => {
+
+  const [items,setItems] = useState([])
+  const [itemIds, setItemIds] = useState(null)
+  const [fetchingDetails, setFetchingDetails] = useState(false)
+  const [currentPage, setCurrentPage] = useState(0);
+  
+  const fetchItems = async (currPage) => {
+    setCurrentPage(currPage)
+    setFetchingDetails(true)
+
+    let itemsList = itemIds
+    if(itemsList === null){
+      const response = await fetch(`${API_ENDPOINT}/jobstories.json`)
+      itemsList = await response.json();
+      setItemIds(itemsList)
+    }
+
+    console.log(itemsList)
+
+    const itemIdsForPage = itemsList.slice(
+      currPage*ITEMS_PER_PAGE,
+      currPage*ITEMS_PER_PAGE + ITEMS_PER_PAGE
+    )
+
+    const itemsForPage = await Promise.all(
+      itemIdsForPage.map(itemId => 
+        fetch(`${API_ENDPOINT}/item/${itemId}.json`).then(res => res.json())
+      )
+    )
+
+    setItems([...items, ...itemsForPage])
+    setFetchingDetails(false)
+  }
+
+  useEffect(() => {
+    if(currentPage == 0) fetchItems(currentPage)
+  },[])
+
+  return (
+    <div className='app'>
+      <h1 className='title'>JOB PORTAL</h1>
+      {
+        items.length < 1 ? ( 
+          <p className='loading'>loading....</p>
+        ) : (
+          <div>
+            <div className='items' role='list'>
+
+              { items.map((item) => { 
+                return <JobPosting key={item.id} {...item} />
+              })}
+
+            </div>
+            <button className='load-more-button' onClick={() => fetchItems(currentPage+1)} disabled={fetchingDetails}>
+              {fetchingDetails? "Loading": "Load more jobs"}
+            </button>
+          </div>
+        )
+      }
+    </div>
+  )
+}
+
+export default App
---
+++ JobBoard/JobBoard/src/components/JobPosting.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const JobPosting = ({ url, title, by, time }) => {
+    const formattedTime = new Date(time*1000).toLocaleString()
+  return (
+    <div className='post' role='listItem'>
+      <h2 className='post__title'>
+        <a className={url ? "" : "inactive"} href={url} target='_blank' rel='noopener'>{title}</a>
+      </h2>
+
+      <span className='post__metadata'>By: {by} {formattedTime}</span>
+    </div>
+  )
+}
+
+export default JobPosting
